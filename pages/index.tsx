ca88819426075625f4a2eaa371af65214f7d4b64
Shane Fitzgerald <shanesrf@gmail.com>
2026-01-15 05:35:55 +1000

    chore: fix lint issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,16 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>DCMCO - Construction Industry AI Consultancy</title>
+        <meta name="robots" content="noindex, nofollow" />
+        <meta
+          name="description"
+          content="DCMCO is a leading AI consultancy specializing in the construction industry"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Hero
         ctaButton={{
           onClick: function Ma() {},
